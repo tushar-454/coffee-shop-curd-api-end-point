3fe9708ca109a3b84ebc264000bfbac935268509
tushar-454 <imtushar454@gmail.com>
2023-10-13 22:10:48 +0600

    there was an mistake in get one coffee by id now fix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
@@ -36,7 +36,7 @@ async function run() {
     // fot get one coffee by id
     app.get('/coffees/:id', async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new Object(id) };
+      const query = { _id: new ObjectId(id) };
       const result = await coffeeDBCollection.findOne(query);
       res.send(result);
     });
